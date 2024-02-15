20dcc7448d2867c65d3ee27a791ad19acb17c899
윤서희 <appleid21@sookmyung.ac.kr>
2024-02-15 21:03:05 +0900

    Style: 온보딩 화면 수정

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
+import HomeIcon from '../assets/home.svg';
 
 const ListPage = () => {
-    return <Header />
+    return (
+        <>
+            <Header />
+            <img src={HomeIcon} alt="Home" style={{ width: 52 }} />
+        </>
+    );
 }
 
 export default ListPage;
--- src/pages/OnBoarding.js
+++ src/pages/OnBoarding.js
@@ -1,31 +1,57 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import Button from '../components/Button';
 import styled from 'styled-components';
+import logo from '../assets/logo.svg';
 
 const Container = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
-  justify-content: flex-end;
+  justify-content: center;
   align-items: center;
-  padding-bottom: 20px;
   width: 100%;
-  box-sizing: border-box;
   height: 100vh;
+  box-sizing: border-box;
+  background-color: #a883d8;
 `;
 
+const LogoImage = styled.img`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 180px;
+  height: auto;
+`;
+
+const Button = styled.button`
+  position: absolute;
+  bottom: 20px;
+  background-color: #ffffff;
+  width: 80%;
+  height: 45px;
+  color: #864ae1;
+  font-family: 'Pretendard', sans-serif;
+  font-weight: 600;
+  font-size: 14px;
+  border: none;
+  border-radius: 16px;
+  cursor: pointer;
+`;
 
 const OnBoarding = () => {
-  const navigator=useNavigate();
-  const goSignUp=()=>{
+  const navigator = useNavigate();
+
+  const goSignUp = () => {
     navigator('/register');
-  }
+  };
 
   return (
     <Container>
-      <Button onClick={goSignUp} title="살아봐유 시작하기" />
+      <LogoImage src={logo} alt="로고" />
+      <Button onClick={goSignUp}>살아봐유 시작하기</Button>
     </Container>
-  )
+  );
 };
 
 export default OnBoarding;
