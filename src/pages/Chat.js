58c5eab7711f6ebd05bc32cfad88718c292efa5a
윤서희 <appleid21@sookmyung.ac.kr>
2024-02-15 20:27:08 +0900

    Style: 채팅 배경색 설정

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -2,14 +2,20 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledChat = styled.div`
-  background-color: #F2E7F9;
+  background-color: #EAE3FC;
+  width: 100%;
+  height: 100%;
+`;
+
+const ChatContainer = styled.div`
+  height: 100vh; /* 100%의 뷰포트 높이 */
 `;
 
 const Chat = () => {
   return (
-    <StyledChat>
-      {/* Chat 컴포넌트의 내용을 추가합니다. */}
-    </StyledChat>
+    <ChatContainer>
+      <StyledChat />
+    </ChatContainer>
   );
 }
 
