45fb439eb0fab6411be7ade4013a99a65dfa146f
윤서희 <appleid21@sookmyung.ac.kr>
2024-02-16 09:19:47 +0900

    Feat: detail api 연결

---
+++ src/services/detail.js
@@ -0,0 +1,36 @@
+export async function detail(house_id) {
+    try {
+        const url = 'https://sarabwayu.com/api/house/detail/' + house_id;
+
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem('token')
+            },
+        });
+
+        const responseData = await response.json();
+
+        if (response.ok) {
+            return {
+                success: true,
+                status_code: response.status,
+                message: responseData.message,
+                data: responseData.data
+            };
+        } else {
+            return {
+                success: false,
+                status_code: response.status,
+                message: responseData.message || '서버 오류가 발생했습니다.'
+            };
+        }
+    } catch (error) {
+        return {
+            success: false,
+            status_code: 500,
+            message: '네트워크 오류가 발생했습니다.'
+        };
+    }
+}
